ada596803391d23eac74d7eeb0458b8130f36612
alina selina <alina.selina08@gmail.com>
2025-05-01 15:46:17 +0300

    init

---
+++ src/main/java/com/example/keycloak/OrganizationDomainMapper.java
@@ -0,0 +1,104 @@
+package com.example.keycloak;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.keycloak.Config;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.common.Profile;
+import org.keycloak.models.ClientSessionContext;
+import org.keycloak.models.KeycloakContext;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.OrganizationModel;
+import org.keycloak.models.ProtocolMapperModel;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.models.utils.ModelToRepresentation;
+import org.keycloak.models.utils.RepresentationToModel;
+import org.keycloak.organization.OrganizationProvider;
+import org.keycloak.protocol.ProtocolMapperUtils;
+import org.keycloak.protocol.oidc.OIDCLoginProtocol;
+import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
+import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
+import org.keycloak.protocol.oidc.mappers.TokenIntrospectionTokenMapper;
+import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
+import org.keycloak.provider.EnvironmentDependentProviderFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.representations.IDToken;
+
+public class OrganizationDomainMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper {
+
+    public static final String PROVIDER_ID = "organization-domain-mapper";
+
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    static {
+        OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, OrganizationDomainMapper.class);
+    }
+
+    @Override
+    public String getDisplayCategory() {
+        return "Token Mapper";
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Organization Domain Mapper";
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Includes organization domain in access token";
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
+
+    @Override
+    protected void setClaim(IDToken token, ProtocolMapperModel model, UserSessionModel userSession,
+                            KeycloakSession session, ClientSessionContext clientSessionCtx) {
+        String orgId = clientSessionCtx.getClientSession().getNote(OrganizationModel.ORGANIZATION_ATTRIBUTE);
+        OrganizationModel organization;
+
+        if (orgId == null) {
+            organization = resolveFromRequestedScopes(session, userSession, clientSessionCtx).findFirst();
+        } else {
+            organization = session.getProvider(OrganizationProvider.class).getById(orgId);
+        }
+
+        if (organization != null) {
+            List<OrganizationDomainModel> domains = organization.getDomains();
+            if (domains != null && !domains.isEmpty()) {
+                String domainName = domains.get(0).getName();
+                OIDCAttributeMapperHelper.mapClaim(token, mappingModel, domainName);
+            }
+        }
+    }
+
+    private Stream<OrganizationModel> resolveFromRequestedScopes(KeycloakSession session, UserSessionModel userSession, ClientSessionContext context) {
+        String rawScopes = context.getScopeString();
+        OrganizationScope scope = OrganizationScope.valueOfScope(session, rawScopes);
+
+        if (scope == null) {
+            return Stream.empty();
+        }
+
+        return scope.resolveOrganizations(userSession.getUser(), rawScopes, session);
+
+    }
+}
